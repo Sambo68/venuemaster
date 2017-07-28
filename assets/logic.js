6d8fdddce6a7e6edf38cee477e6c9c0f8726a734
joe-fuentes <joevfuentes@hotmail.com>
2017-07-27 21:07:08 -0400

    Added 'fierbase.database()' function to logic.js

--- assets/logic.js
+++ assets/logic.js
@@ -9,29 +9,31 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
 var geocoder;
-  var map;
-  function initMap() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(40.7127837, -74.0059413);
-    var mapOptions = {
-      zoom: 14,
-      center: latlng
-    }
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  }
+var map;
 
-  function codeAddress() {
-    var address = document.getElementById('address').value;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == 'OK') {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
+function initMap() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(40.7127837, -74.0059413);
+  var mapOptions = {
+    zoom: 14,
+    center: latlng
   }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
