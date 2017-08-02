9adeabecb96a52973607cf9ed72ad202d0693f92
Zachary Saltzman <zsaltzman@Zacharys-MacBook-Pro-2.local>
2017-08-02 18:14:35 -0400

    Email validation

--- script/script.js
+++ script/script.js
@@ -317,14 +317,27 @@
 
 		var emailAdd = $("#emailAdd").val().trim();
 
-		emailjs.send("gmail","events_template",{
-  			to_email: emailAdd, 
-  			html: tableHTML
-		});
+		function validateForm() {
+    		var x = document.forms["myForm"]["email"].value;
+    		var atpos = x.indexOf("@");
+    		var dotpos = x.lastIndexOf(".");
+    		if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        		alert("Not a valid e-mail address");
+        		return false;
+    		}
+    		else {
+    			emailjs.send("gmail","events_template",{
+  				to_email: emailAdd, 
+  				html: tableHTML
+				});
+		    	alert("E-mail Sent!");
+		    	return true;
+    		}
+		}
+
+		validateForm();
 
 		$("#emailAdd").val("");
-
-		alert("E-mail Sent!");
 	});
 
 
